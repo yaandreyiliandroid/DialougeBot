e9aaa591e587df02e6870117f43de4207e90c7ad
yaandreyiliandroid <133146230+yaandreyiliandroid@users.noreply.github.com>
2023-12-01 21:52:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -23,15 +23,36 @@ async def hello(ctx):
     await ctx.send(f'Привет! Я Диалоговый Бот!')
 
 @bot.command()
-async def help(ctx):
-    await ctx.send(f'Я имею команды:
-                   Диалоговые:
-                   1)h.a.y-спрашивание как дела
-                   2)w.y.s.m.a.s-советует один сервер
-                   3)wahtdoyodo?-спрашивание что делаешь
-                   Советные(советы по теме глобального потепления):
-                   1)w.t.d.i.i.s.t-что делать если увидел мусор
-                   2)stop.glob.warm-как остановить потепление
-                   Приятного использованию Диалоговым ботом:)')
+async def info(ctx):
+    await ctx.send(f'$hay-как дела?
+                   $hello-привет
+                   $goodbye-пока
+                   $wwyd-что ты будешь делать?
+                   $isawtrsh-расказывает что делать если увидел мусор
+                   $stpgp-советы при глобальном потеплении')
+
+@bot.command()
+async def hay(ctx):
+    await ctx.send(f'норм с тобой общаюсь')
+
+@bot.command()
+async def goodbye(ctx):
+    await ctx.send(f'Пока')
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged in as {self.user} (ID: {self.user.id})')
+        print('------')
+    async def on_member_join(self, member):
+        guild = member.guild
+        if guild.system_channel is not None:
+            to_send = f'Привет {member.mention} Добро пожаловать в {guild.name}! Я диалоговый бот! напиши команду $info чтобы узнать какие команды уменя есть'
+            await guild.system_channel.send(to_send)
+
+
+intents = discord.Intents.default()
+intents.members = True
+
+client = MyClient(intents=intents)
 
 bot.run("token")
